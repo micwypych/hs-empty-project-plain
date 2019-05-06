485f9362e3f11ccbeca6e5ca0072b8e1c74efea0
Michał Wypych <micwypych@gmail.com>
2019-05-06 02:10:50 +0200

    Adds repository to the project

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Student
+import StudentRepository
+import Database.HDBC.Sqlite3
 
 main :: IO ()
-main = someFunc
+main = do
+    connection <- connectSqlite3 "test1.db"
+    migrateStudents connection
+    repository <- mkStudentRepository connection
+
+    let Just student = mkStudent "Abc" "Efg" "00000000"
+
+    result <- createStudent repository student
+    print result
+
+    let Just student = mkStudent "Abc2" "Efg2" "11111111"
+    result' <- createStudent repository student
+    print result'
--- src/Student.hs
+++ src/Student.hs
@@ -1,9 +1,29 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Student where
 
 import Data.Text
 
-data Studnet = Student FirstName LastName StudentId
+data Student = Student FirstName LastName StudentId
 
 newtype FirstName = FirstName Text
 newtype LastName = LastName Text
-newtype StudentId = StudentId Int
+newtype StudentId = StudentId Text
+
+mkStudent :: Text -> Text -> Text -> Maybe Student
+mkStudent firstName lastName studentId = 
+    Student <$> validate firstName
+            <*> validate lastName
+            <*> validate studentId
+ 
+class Validate fromType toType where
+    validate :: fromType -> Maybe toType
+
+instance Validate Text FirstName where
+    validate firstName = Just $ FirstName firstName
+
+
+instance Validate Text LastName where
+    validate lastName = Just $ LastName lastName
+
+instance Validate Text StudentId where
+    validate studentId = Just $ StudentId studentId
